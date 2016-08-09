47026665f8a6b117f0d33a2349572eb36fe8639d
Michael Mikhalevich <mike@viber.com>
2016-08-10 00:18:07 +0300

    First commit

---
+++ auth.go
@@ -0,0 +1,61 @@
+package mgoauth
+
+import (
+	//"gopkg.in/mgo.v2"
+	//"gopkg.in/mgo.v2/bson"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+var (
+	authTemplate = template.Must(template.New("Auth").ParseFiles("templates/Login.html", "templates/Test.html"))
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		if err := authTemplate.ExecuteTemplate(w, "Login.html", nil); err != nil {
+			panic(err)
+		}
+
+	case "POST":
+		Auth(w, r)
+	}
+}
+
+func Auth(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("name")
+	password := r.FormValue("password")
+
+	if username == "guest" && password == "guest123" {
+		expire := time.Now().Add(5 * time.Minute)
+		cookie := http.Cookie{Name: "SessionID", Value: "signin", Expires: expire, HttpOnly: true}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, "/", http.StatusFound)
+	} else {
+		http.Redirect(w, r, "/login", http.StatusMovedPermanently)
+	}
+}
+
+func CheckAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorized := false
+		cookie, err := r.Cookie("SessionID")
+		if err == nil && cookie.Value == "signin" {
+			authorized = true
+		}
+
+		if !authorized && r.URL.Path != "/login" {
+			http.Redirect(w, r, "/login", http.StatusFound)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func Test(w http.ResponseWriter, r *http.Request) {
+	if err := authTemplate.ExecuteTemplate(w, "Test.html", nil); err != nil {
+		panic(err)
+	}
+}
