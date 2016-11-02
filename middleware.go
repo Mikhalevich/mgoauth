053ec56532dd42c7d99ca2e54e2a0b69fc248b97
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2016-11-03 00:22:09 +0300

    CheckAuth middleware function was corrected

--- middleware.go
+++ middleware.go
@@ -9,39 +9,57 @@ import (
 func CheckAuth(next http.Handler, role int) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authorized := false
+		defer func() {
+			if authorized {
+				next.ServeHTTP(w, r)
+			} else {
+				http.Redirect(w, r, UrlLoginPage, http.StatusFound)
+			}
+		}()
+
 		pagePriority, ok := RolePriority[role]
 		if !ok {
 			log.Println("Invalid handler role")
-		} else if role == EmptyRole {
+			return
+		}
+
+		if role == EmptyRole {
 			authorized = true
-		} else {
-			if cookie, err := r.Cookie(SessionIdName); err == nil {
-				sessionId := cookie.Value
-				storage := NewStorage()
-				defer storage.Close()
-
-				if user, err := storage.UserBySessionId(sessionId); err == nil {
-					userPriority, ok := RolePriority[user.Role]
-					if !ok {
-						log.Println("Invalid user role")
-					} else if userPriority < pagePriority {
-						log.Println("Not allower for current user")
-					} else {
-						if user.SessionExpires < time.Now().Unix() {
-							log.Println("Session was expired")
-						} else {
-							authorized = true
-							setCurrentUser(r, user)
-						}
-					}
-				}
-			}
+			return
 		}
 
-		if authorized {
-			next.ServeHTTP(w, r)
-		} else {
-			http.Redirect(w, r, UrlLoginPage, http.StatusFound)
+		cookie, err := r.Cookie(SessionIdName)
+		if err != nil {
+			return
 		}
+
+		sessionId := cookie.Value
+		storage := NewStorage()
+		defer storage.Close()
+
+		user, err := storage.UserBySessionId(sessionId)
+		if err != nil {
+			log.Println("No such session")
+			return
+		}
+
+		userPriority, ok := RolePriority[user.Role]
+		if !ok {
+			log.Println("Invalid user role")
+			return
+		}
+
+		if userPriority < pagePriority {
+			log.Println("Not allower for current user")
+			return
+		}
+
+		if user.SessionExpires < time.Now().Unix() {
+			log.Println("Session was expired")
+			return
+		}
+
+		authorized = true
+		setCurrentUser(r, user)
 	})
 }
